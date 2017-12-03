9d30784a3cc2145eb1db9b8b85fff815f5d87802
Melanie Uba <melanie.uba@gmail.com>
2017-12-03 21:17:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function happyHolidaysTo(name){
   console.log("Happy holidays,${name}")
   return `Happy holidays, ${name}!`
 }
-function happyHolidayTo (holiday name){
+function happyHolidayTo (name){
   console.log("${holiday}")
   console.log("${name}")
   return `Happy ${holiday}, ${name}!`
